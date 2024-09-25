c9ac48d752c38813f25b57fcf1b3553fbc838d91
Khoi Nguyen <khoinguyenfc123@gmail.com>
2024-09-26 04:25:25 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -7,6 +8,16 @@ const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+	return next(new ApiError(404, "resource not found"));
+})
+
+app.use((err, req, res, next) => {
+	return res.status(error.statusCode || 500).json({
+		message: error.message || "Internal Server Error",
+	});
+});
 
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to contact book application."});
